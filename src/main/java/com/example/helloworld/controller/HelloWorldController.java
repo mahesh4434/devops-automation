d2c0dba434ab7b0a369b00f31ac1df53094f12bc
mahesh4434 <maheshchaudhari511@gmail.com>
2023-10-17 22:06:30 +0530

    login page added

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,13 +1,13 @@
 package com.example.helloworld.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/login")
     public String sendGreetings() {
-        return "Hello, World!";
+        return "login";
     }
 }
